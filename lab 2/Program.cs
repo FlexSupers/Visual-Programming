3dfaf8116360fd6c28b3d72ce82373336f8c2bbb
FlexSupers <flexsupers@gmail.com>
2022-02-24 22:04:41 +0700

    lab 2

---
+++ lab 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            const int size = 10;
+
+            RomanNumber[] numbers = new RomanNumber[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                int tmp = rand.Next();
+                ushort x = (ushort)tmp;
+                numbers[i] = new RomanNumber(x);
+                Console.WriteLine(numbers[i].ToString() + " " + x);
+            }
+
+            Array.Sort<RomanNumber>(numbers);
+            Console.WriteLine();
+
+            foreach (RomanNumber i in numbers)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
---
+++ lab 2/RomanNumberException.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+internal class RomanNumberException : Exception
+{
+    /* Исключения для класса TO DO */
+}
